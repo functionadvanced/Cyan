d1643f2dd971828ee43f3350ddf968f492aa8efa
Peizhong Ju <jupeizhong6@gmail.com>
2018-11-15 14:20:22 -0500

    LSTM

--- LSTM.py
+++ LSTM.py
@@ -2,13 +2,14 @@ import torch
 import torch.utils.data
 import MidiPoint
 
+torch.manual_seed(1)
+
 class LSTMpredictor(torch.nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, note_num, target_dim):
+    def __init__(self, hidden_dim, note_num):
         super(LSTMpredictor, self).__init__()
         self.hidden_dim = hidden_dim
-        self.lstm = torch.nn.LSTM(embedding_dim, hidden_dim)
-        self.note_embedding = torch.nn.Embedding(note_num, embedding_dim)
-        self.hidden2note = torch.nn.Linear(hidden_dim, target_dim)
+        self.lstm = torch.nn.LSTM(2, hidden_dim)
+        self.hidden2note = torch.nn.Linear(hidden_dim, 2)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
@@ -16,40 +17,97 @@ class LSTMpredictor(torch.nn.Module):
                 torch.zeros(1, 1, self.hidden_dim))
 
     def forward(self, note_seq):
-        embeds = self.note_embedding(note_seq)
-        lstm_out, self.hidden = self.lstm(embeds.view(len(note_seq), 1, -1), self.hidden)
+        lstm_out, self.hidden = self.lstm(note_seq.view(len(note_seq), 1, 2), self.hidden)
         note_space = self.hidden2note(lstm_out.view(len(note_seq), -1))
         note_scores = torch.nn.functional.log_softmax(note_space, dim=1)
-        return note_scores
+        return note_space
 
 class DataSet(torch.utils.data.Dataset):
+    def normalize(self):
+        # find the min note & max delta_time, then normalize
+        self.min_note = 1000
+        self.max_note = 0
+        self.max_delta_time = 0
+        for i in range(len(self.pointList)):
+            if self.pointList[i].note < self.min_note:
+                self.min_note = self.pointList[i].note
+            if self.pointList[i].note > self.max_note:
+                self.max_note = self.pointList[i].note
+            if self.pointList[i].delta_time > self.max_delta_time:
+                self.max_delta_time = self.pointList[i].delta_time
+        
+        for i in range(len(self.pointList)):
+            self.pointList[i].note -= self.min_note
+            self.pointList[i].delta_time *= 3
+    
+    def recover(self, aim): # aim should be a n*2 tensor
+        aim[:, 0] += self.min_note
+        aim[:, 1] /= 3
+        return aim
+
     def __init__(self, mode, isTrain=False):
         self.isTrain = isTrain
         if mode == 0: # both hands
-            self.pointList = MidiPoint.PointList('1-l.mid', '1-r.mid').list
+            self.pointList = MidiPoint.PointList('2-l.mid', '2-r.mid').list
         elif mode == 1: # left hand
-            self.pointList = MidiPoint.PointList('1-l.mid').list
+            self.pointList = MidiPoint.PointList('2-l.mid').list
         else:
-            self.pointList = MidiPoint.PointList('1-r.mid').list
-    def __len__(self):
-        return 1
-    def __getitem__(self, idx):
+            self.pointList = MidiPoint.PointList('2-r.mid').list
+
+        self.normalize()
+
         offset = 0
-        total_len = int(len(self.pointList) * 0.8) - 1
+        total_len = 40
         if not self.isTrain:
             offset = int(len(self.pointList) * 0.8)
             total_len = int(len(self.pointList) * 0.2) - 1
-        result = []
-        label = []
+        self.result = []
+        self.label = []
+
+        
+
         for i in range(total_len):
             temp = offset+i
-            result.append((self.pointList[temp].note, self.pointList[temp].delta_time))
-            label.append((self.pointList[temp+1].note, self.pointList[temp+1].delta_time))
-        
-        return (result, label)
+            self.result.append([self.pointList[temp].note, self.pointList[temp].delta_time])
+            self.label.append([self.pointList[temp+1].note, self.pointList[temp+1].delta_time])
 
-train_dataset = DataSet(0,isTrain=True)
-train_loader = torch.utils.data.DataLoader(train_dataset)
-for (i, j) in train_loader:
-    print(i[10])
-    break
+
+    def __len__(self):
+        return 1
+    def __getitem__(self, idx):       
+        return (self.result, self.label)
+
+
+def PlayResult(re):
+    pl = []
+    for i in re:
+        if i[1] < 0:
+            i[1] = 0
+        pl.append(MidiPoint.Point(int(torch.round(i[0])), 100, 0, 0, i[1]))
+    MidiPoint.PlayList(pl)
+
+train_dataset = DataSet(2,isTrain=True)
+myLstm = LSTMpredictor(64, len(train_dataset.result))
+loss_func = torch.nn.MSELoss(reduction="sum")
+optimizer = torch.optim.SGD(myLstm.parameters(), lr=0.001)
+data = torch.tensor(train_dataset.result)
+target = torch.tensor(train_dataset.label)
+
+for epoch in range(2000):
+    myLstm.zero_grad()
+    myLstm.hidden = myLstm.init_hidden()
+    predict = myLstm(data)
+    loss = loss_func(predict, target)
+    print(loss)
+    loss.backward(retain_graph=True)
+    optimizer.step()
+    # print(myLstm(data))
+with torch.no_grad():
+    # print(train_dataset.recover(target))
+    myLstm.hidden = myLstm.init_hidden()
+    
+    re = train_dataset.recover(myLstm(data))
+    print(re)
+    # PlayResult(train_dataset.recover(target))
+    PlayResult(re)
+
--- MidiPoint.py
+++ MidiPoint.py
@@ -14,6 +14,37 @@ class Point:
             temp = 'right'
         return "note="+str(self.note)+" velocity="+str(self.velocity)+" time="+str(self.time)+" delta_time="+str(self.delta_time)+" "+temp
 
+def PlayList(point_list, newMidiname="temp.mid", play=True, mode=0):
+    '''
+    mode=0: play both hands
+    mode=1: play left hand
+    mode=2: play right hand
+    '''
+    mid = mido.MidiFile()
+    track = mido.MidiTrack()
+    mid.tracks.append(track)
+    track.append(mido.Message('program_change', program=0, time=0))
+    
+    for m in point_list:
+        if m.isLeft:
+            if mode == 2:
+                continue
+        else:
+            if mode == 1:
+                continue
+        # one second is 960 ticks
+        track.append(mido.Message('note_on', note=m.note, velocity=m.velocity, time=int(m.delta_time*960)))
+        
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filePath = os.path.join(dir_path, 'midi_files', newMidiname)
+    mid.save(filePath) # save as midi file
+    if play:
+        import pygame
+        pygame.init()
+        pygame.mixer.music.load(filePath)
+        pygame.mixer.music.play()
+        input("press to exit")
+
 class PointList:
     def __init__(self, l_file_name, r_file_name=None):
         self.list = []
@@ -21,6 +52,12 @@ class PointList:
         if r_file_name != None:
             self.add(r_file_name, False)
         self.list.sort(key=lambda x: x.time)
+        self.calDeltaTime()
+
+    def calDeltaTime(self):
+        for i in range(len(self.list)-1):
+            self.list[i+1].delta_time = self.list[i+1].time - self.list[i].time
+
     def add(self, file_name, isLeft):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         filePath = os.path.join(dir_path, 'midi_files', file_name)
@@ -33,35 +70,9 @@ class PointList:
                     self.list.append(Point(msg.note, msg.velocity, current_time, isLeft, msg.time))
 
     def saveAsMidi(self, newMidiname, play=False, mode=0):
-        '''
-        mode=0: play both hands
-        mode=1: play left hand
-        mode=2: play right hand
-        '''
-        mid = mido.MidiFile()
-        track = mido.MidiTrack()
-        mid.tracks.append(track)
-        track.append(mido.Message('program_change', program=0, time=0))
-        
-        for m in self.list:
-            if m.isLeft:
-                if mode == 2:
-                    continue
-            else:
-                if mode == 1:
-                    continue
-            # one second is 960 ticks
-            track.append(mido.Message('note_on', note=m.note, velocity=m.velocity, time=int(m.delta_time*960)))
-            
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        filePath = os.path.join(dir_path, 'midi_files', newMidiname)
-        mid.save(filePath) # save as midi file
         if play:
-            import pygame
-            pygame.init()
-            pygame.mixer.music.load(filePath)
-            pygame.mixer.music.play()
-            input("press to exit")
+            PlayList(self.list, newMidiname=newMidiname, play=play, mode=mode)
+        
     
 # pl = PointList('1-l.mid', '1-r.mid')
 # pl.saveAsMidi('temp.mid', play=True)
